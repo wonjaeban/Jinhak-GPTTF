18c38780b6d694c124741e211673ed0bd96daf23
반 원재 <wonjae0709@jinhakapply.com>
2024-10-26 17:28:45 +0900

    feat: testtest2222

---
+++ temp.ts
@@ -0,0 +1,12 @@
+// example.js
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+console.log("Addition:", add(5, 3));
+console.log("Subtraction:", subtract(5, 3));
